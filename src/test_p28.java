aa040a7362bdfe1e9f4557f5da22827e0323fd32
xiu1021 <t109360151@ntut.org.tw>
2022-10-24 15:56:18 +0800

    edit

---
+++ src/test_p28.java
@@ -0,0 +1,12 @@
+
+public class test_p28 {
+	public static void main(String[] args) 
+	{
+		int i=1;
+		do {
+			System.out.println("第"+i+"次迴圈");
+			i++;
+		}while(i<=5);
+		System.out.println("迴圈結束");
+	}
+}
